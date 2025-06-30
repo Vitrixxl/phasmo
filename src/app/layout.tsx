a277a70afdfc53c46fe6f864e0972f7d4f0ddcf5
Vitrixxl <vitrice91@gmail.com>
2025-06-30 11:00:43 +0200

    ban entity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,16 +20,19 @@ export const AppLayout = () => {
     if (huntSpeeds.length > 0 && !huntSpeeds.includes(entityHuntSpeed)) {
       possible = false;
     }
+    console.log({ selectedProofs, bannedProofs });
     for (const p of selectedProofs) {
       if (!entityProofs.includes(p)) possible = false;
     }
     for (const p of bannedProofs) {
+      console.log(possible);
       if (entityProofs.includes(p)) possible = false;
     }
     if (
       entityName == 'Mimic' && selectedProofs.includes('Orbe fantomatique') &&
       !selectedProofs.includes('D.O.T.S.')
     ) {
+      console.log(possible);
       return true;
     }
     return possible;
--- src/components/entity-card.tsx
+++ src/components/entity-card.tsx
@@ -1,9 +1,9 @@
 import { type Entity, type Proof } from '@/lib/game-data';
-import { LucideCheck } from 'lucide-react';
 import { useNavigate, useParams, useSearchParams } from 'react-router';
 import { Badge } from './ui/badge';
 import { cn } from '@/lib/utils';
 import { useProofs } from '@/hooks/use-proofs';
+import { useState } from 'react';
 
 type EntityCardProps = {
   entity: Entity;
@@ -14,30 +14,39 @@ type EntityCardProps = {
 export const EntityCard = (
   { entity, possible }: EntityCardProps,
 ) => {
+  const [isDitched, setIsDitched] = useState(false);
   const navigate = useNavigate();
   const { bannedProofs, selectedProofs } = useProofs();
   const [searchParams] = useSearchParams();
   const { name } = useParams();
   return (
-    <button
-      onClick={() => navigate(entity.name + '?' + searchParams.toString())}
+    <div
+      onContextMenu={(e) => {
+        e.preventDefault();
+        setIsDitched((prev) => !prev);
+      }}
+      onClick={(e) => {
+        e.stopPropagation();
+        navigate(entity.name + '?' + searchParams.toString());
+      }}
       className={cn(
-        'p-4 bg-card border rounded-lg flex flex-col gap-4',
-        possible ? '' : 'opacity-40',
-        name == entity.name ? 'border-border-focused' : '',
+        'p-4 bg-card border rounded-lg flex flex-col gap-4 transition-colors',
+        !possible && 'opacity-40',
+        name == entity.name && 'border-border-focused',
+        isDitched && 'border-destructive ring-destructive/50 ring-2 opacity-40',
       )}
     >
       <div className='flex gap-4 items-center'>
         <div className='rounded-md bg-primary border size-10' />
-        <p className='font-semibold text-lg'>{entity.name}</p>
-        {selectedProofs.length == 3 && possible && (
-          <LucideCheck className=' ml-auto text-violet-600 stroke-[4]' />
-        )}
+        <p className={'font-semibold text-lg'}>
+          {entity.name}
+        </p>
       </div>
       <div className='w-full border-t' />
-      <div className='flex flex-wrap items-center justify-center gap-2 h-full'>
+      <div className='flex flex-wrap gap-2 h-full'>
         {entity.proofs.map((p, i) => (
           <Badge
+            className='h-fit'
             variant={selectedProofs.includes(p)
               ? 'primary'
               : bannedProofs.includes(p)
@@ -49,6 +58,6 @@ export const EntityCard = (
           </Badge>
         ))}
       </div>
-    </button>
+    </div>
   );
 };
